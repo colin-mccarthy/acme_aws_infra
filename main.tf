9c0b823b7617ebd7bfc52f5fb7e40e5fb948892e
Colin McCarthy <colin-mccarthy@users.noreply.github.com>
2022-09-19 14:14:20 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,68 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_instance" "ubuntu_server" {
+  ami                    = "ami-052efd3df9dad4825"
+  instance_type          = "t3.micro"
+  key_name               = "terra"
+  vpc_security_group_ids = [aws_security_group.ubuntu_servers.id]
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+
+variable "my_vpc" {
+  type = string
+}
+
+resource "aws_security_group" "ubuntu_servers" {
+  name        = "ubuntu_servers"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = var.my_vpc
+
+  egress = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+ ingress                = [
+   {
+     cidr_blocks      = [ "0.0.0.0/0", ]
+     description      = ""
+     from_port        = 22
+     ipv6_cidr_blocks = []
+     prefix_list_ids  = []
+     protocol         = "tcp"
+     security_groups  = []
+     self             = false
+     to_port          = 22
+  }
+  ]
+}
+
+
+output "instance_ip_addr" {
+  value = aws_instance.ubuntu_server.public_dns
+}

255f4e5dd887e689388befa03a0939b65e8ab69f
Colin McCarthy <colin-mccarthy@users.noreply.github.com>
2022-09-19 22:40:46 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ module "ec2_instances" {
 
   ami                    = "ami-052efd3df9dad4825"
   instance_type          = "t3.micro"
-  vpc_security_group_ids = [sg]
+  vpc_security_group_ids = [module.sg.security_group_id]
   key_name               = "terra"
 
   tags = {
